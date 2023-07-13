17b676307667ff5f3722e8c4ee37c8b53d1193be
qihang <a1091735073@163.com>
2023-07-13 12:38:44 +0800

    1

--- src/pages/pc/NewsInformation/index.jsx
+++ src/pages/pc/NewsInformation/index.jsx
@@ -1,10 +1,35 @@
 // 新闻资讯
+import React from "react";
+import { Image } from 'antd';
 
 const NewsInformation = () => {
   return (
-    <>
-      <div>新闻资讯</div>
-    </>
+    <div className="news-container">
+      <header>
+        <img 
+          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+        />
+      </header>
+      <section>
+        <h5></h5>
+        <div>
+          <aside>
+            <h6></h6>
+            <article></article>
+          </aside>
+          <figure>
+            <img src="" alt="" srcset="" />
+          </figure>
+        </div>
+      </section>
+      <section>
+        <article>
+          <h6></h6>
+          <span></span>
+        </article>
+        <video src=""></video>
+      </section>
+    </div>
   )
 }
 
